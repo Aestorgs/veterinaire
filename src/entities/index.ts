c62372b1e326fdc42c8c0687c159e4eea369838e
Dylan R <90897072+Aestorgs@users.noreply.github.com>
2022-12-19 14:08:23 +0100

    Update index.ts

--- src/entities/index.ts
+++ src/entities/index.ts
@@ -3,6 +3,6 @@ import { Utilisateur } from './user.entity';
 import { Types } from './type.entity';
 import { Pets } from './pets.entity';
 
-// il affiche les tables dans postgres 
+// j'affiche les tables dans postgres 
 const entities = [Clients, Utilisateur, Types, Pets];
 export default entities;
