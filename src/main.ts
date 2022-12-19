05f2e7812c8106eb08e8e06256d952fb925a107d
Dylan R <90897072+Aestorgs@users.noreply.github.com>
2022-12-19 14:19:01 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+//j'écoute sur le ports 3000
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
